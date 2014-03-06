334749cf9925eca1c99dba9f7e61aa5f0263663f
chrisw <chrisw@soe.ucsc.edu>
2014-03-06 12:03:56 -0800

    static file handler

--- bmeg_server.py
+++ bmeg_server.py
@@ -6,10 +6,11 @@ import tornado.web
 
 class MainHandler(tornado.web.RequestHandler):
     def get(self):
-		self.write("Hello, world")
+		self.write("Hello, world.  This is the MainHandler.")
 
 application = tornado.web.Application([
 	(r"/", MainHandler),
+	(r"/static/(.*)", tornado.web.StaticFileHandler, {"path": r"static/"}),
 ])
 
 if __name__ == "__main__":
