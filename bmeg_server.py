1ef3c4276625b992c0802e231d1036d9399b8b0c
chrisw <chrisw@soe.ucsc.edu>
2014-03-06 16:44:43 -0800

    trying bulbs python module

--- bmeg_server.py
+++ bmeg_server.py
@@ -7,6 +7,10 @@ import tornado.autoreload
 import datetime
 import urllib2
 import json
+# from bulbs.config import Config, DEBUG
+# from bulbs.rexster import RexsterClient
+import bulbs.config
+import bulbs.rexster
 
 def getTime():
 	now = datetime.datetime.now()
@@ -17,29 +21,52 @@ def prettyJson(object):
 	s = json.dumps(jo, sort_keys=True, indent=4, separators=(',', ': '))
 	return s
 
-def queryBmeg():
-	# http://localhost:8182/graphs/graph/
-	url = "http://localhost:8182/graphs/graph"
-	response = urllib2.urlopen(url).read()
-	sys.stderr.write("response\t" + prettyJson(response) + "\n")
-	return prettyJson(response)
-
 class MainHandler(tornado.web.RequestHandler):
     def get(self):
 		self.write("Hello, world.  This is the MainHandler.")
 
+# use https://github.com/tinkerpop/rexster/wiki/Basic-REST-API to query the graph
+def queryBmeg():
+	# http://localhost:8182/graphs/graph/
+	baseUrl = "http://localhost:8182"
+# 	query = "/graphs/graph/vertices?key=type&value=tcga_attr:Patient"
+	query = "/graphs/graph/vertices/348464/outE?_label=tcga_attr:gender/in"
+	url = baseUrl + query
+	response = urllib2.urlopen(url).read()
+	sys.stderr.write("response\t" + prettyJson(response) + "\n")
+	return prettyJson(response)
+
 class BmegGremlinQueryHandler(tornado.web.RequestHandler):
 	def get(self):
-		self.write("This is the BmegGremlinQueryHandler.\n")
+		self.write("This is the BmegGremlinQueryHandler.<hr>")
 		response = queryBmeg()
 		sys.stderr.write(str(getTime()) + "\t" + "" + response + "\n")
-		self.write(str(getTime()) + "\t" + "" + response + "\n")
+		self.write(str(getTime()) + "<hr>")
+		self.write(response + "<hr>")
+
+def queryBmeg_2():
+	config = bulbs.config.Config("http://localhost:8182/graphs/graph")
+	config.set_logger(bulbs.config.DEBUG)
+	client = bulbs.rexster.client.RexsterClient(config)
+# 	script = client.scripts.get(r"g.v(348464)")
+# 	rexster_response = client.gremlin(script, params=None)
+	rexster_response = client.get_vertex(348464)
+	rexster_results = rexster_response.get_results()
+	return rexster_results
+
+class BmegGremlinQueryHandler2(tornado.web.RequestHandler):
+	def get(self):
+		self.write("This is the BmegGremlinQueryHandler2.<hr>")
+		rexster_results = queryBmeg_2()
+		for results in rexster_results:
+			sys.stderr.write("id " + str(results.get_id()) + "\n")
 
 # map urls to handlers
 application = tornado.web.Application([
 	(r"/", MainHandler),
 	(r"/static/(.*)", tornado.web.StaticFileHandler, {"path": r"static/"}),
-	(r"/query", BmegGremlinQueryHandler)
+	(r"/query", BmegGremlinQueryHandler),
+	(r"/query2", BmegGremlinQueryHandler2)
 ])
 
 # start server
