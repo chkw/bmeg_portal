f7610300fb99da1a6099d56c15f3a6c41ae97519
chrisw <chrisw@soe.ucsc.edu>
2014-03-07 17:20:32 -0800

    a module for handling bmeg query

---
+++ static/data_summary/scripts/bmeg_query.js
@@ -0,0 +1,14 @@
+/**
+ * chrisw@soe.ucsc.edu
+ * March 7, 2014
+ * bmeg_query.js contains methods to make queries to BMEG and parse the response.
+ */
+
+/**
+ * Read this: http://blog.getify.com/native-javascript-sync-async/
+ * @param {Object} script
+ * @param {Object} callback
+ */
+function queryBmeg(script, callback) {
+
+}
