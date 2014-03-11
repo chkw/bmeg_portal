6626722e26152b3e097a49fa05f1e77ad45e4e9f
chrisw <chrisw@soe.ucsc.edu>
2014-03-11 12:30:51 -0700

    asynch get request

--- static/data_summary/scripts/bmeg_query.js
+++ static/data_summary/scripts/bmeg_query.js
@@ -4,11 +4,31 @@
  * bmeg_query.js contains methods to make queries to BMEG and parse the response.
  */
 
+var bmeg_service_host = "http://localhost:9886";
+
 /**
  * Read this: http://blog.getify.com/native-javascript-sync-async/
+ * https://developer.mozilla.org/en-US/docs/Web/API/XMLHttpRequest/Using_XMLHttpRequest
  * @param {Object} script
  * @param {Object} callback
  */
-function queryBmeg(script, callback) {
+function queryBmeg(script, successCallback) {
 
+    var query_uri_base = bmeg_service_host + "/query?script=";
+
+    var xhr = new XMLHttpRequest();
+    xhr.open("GET", query_uri_base + script, true);
+    xhr.onload = function(e) {
+        if (xhr.readyState === 4) {
+            if (xhr.status === 200) {
+                successCallback(xhr.responseText);
+            } else {
+                console.error(xhr.statusText);
+            }
+        }
+    };
+    xhr.onerror = function(e) {
+        console.error(xhr.statusText);
+    };
+    xhr.send(null);
 }
